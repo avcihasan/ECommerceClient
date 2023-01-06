8e3dfa5643d48e78d4dbbc3f1b0f5caa6866a635
avcihasan <hsnavci7@gmail.com>
2023-01-06 04:17:57 +0300

    commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { HomeComponent } from './ui/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      {
+        path: 'customers',
+        loadChildren: () =>
+          import('../app/admin/components/customers/customers.module').then(
+            (m) => m.CustomersModule
+          ),
+      },
+      {
+        path: 'products',
+        loadChildren: () =>
+          import('../app/admin/components/products/products.module').then(
+            (m) => m.ProductsModule
+          ),
+      },
+      {
+        path: 'orders',
+        loadChildren: () =>
+          import('../app/admin/components/orders/orders.module').then(
+            (m) => m.OrdersModule
+          ),
+      },
+    ],
+  },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
